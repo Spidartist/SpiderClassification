c1d10440e25c2d5edb720cf0a4026dff781050b4
Spidartist <hoangdanhquan48@gmail.com>
2022-11-30 09:02:18 +0700

    new model

--- read_dataset.py
+++ read_dataset.py
@@ -3,7 +3,7 @@ import random
 import tensorflow as tf
 import cv2
 import os
-from keras.layers import Conv2D, Dense, MaxPool2D, Input, Flatten
+from keras.layers import Conv2D, Dense, MaxPool2D, Input, Flatten, BatchNormalization, Dropout
 from keras.models import Model
 from keras.utils import plot_model
 from keras.metrics import Recall, Precision
@@ -14,6 +14,7 @@ labels = sorted(os.listdir("./dataset/train"))
 char_to_int = dict((c, i) for i, c in enumerate(labels))
 onehot_encoded = dict()
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 def get_path_X(type_data):
     data_path = []
@@ -82,45 +83,46 @@ def build_model():
     size = 224
     inputs = Input((size, size, 3))
     x = inputs
-    x = Conv2D(16, (3, 3), padding="same", activation="relu")(x)
-    x = MaxPool2D((2, 2))(x)
-    x = Conv2D(32, (3, 3), padding="same", activation="relu")(x)
-    x = MaxPool2D((2, 2))(x)
-    x = Conv2D(64, (3, 3), padding="same", activation="relu")(x)
-    x = MaxPool2D((2, 2))(x)
-    x = Conv2D(64, (3, 3), padding="same", activation="relu")(x)
-    x = MaxPool2D((2, 2))(x)
+    x = Conv2D(64, (3, 3), activation="relu")(x)
+    x = Conv2D(128, (3, 3), activation="relu")(x)
+    x = MaxPool2D((2, 2), strides=2)(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.3)(x)
     x = Flatten()(x)
+    x = Dense(1024, activation="relu")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.3)(x)
     x = Dense(len(labels), activation="softmax")(x)
     return Model(inputs, x)
 
 
-batch = 8
-lr = 1e-4
-epochs = 50
+if __name__ == "__main__":
+    batch = 8
+    lr = 1e-4
+    epochs = 50
 
-cnn_model = build_model()
-plot_model(cnn_model, "model.png", show_shapes=True)
-cnn_model.summary()
-metrics = ["acc", Recall(), Precision()]
-cnn_model.compile(loss="binary_crossentropy", optimizer="adam", metrics=metrics)
-callbacks = [
-    ModelCheckpoint("files/model_new.h5"),
-    EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=False)
-]
-train_steps = len(train_X_path) // batch
-valid_steps = len(val_X_path) // batch
-if len(train_X_path) % batch != 0:
-    train_steps += 1
-if len(val_X_path) % batch != 0:
-    valid_steps += 1
+    cnn_model = build_model()
+    plot_model(cnn_model, "model.png", show_shapes=True)
+    cnn_model.summary()
+    metrics = ["acc", Recall(), Precision()]
+    cnn_model.compile(loss="binary_crossentropy", optimizer="adam", metrics=metrics)
+    callbacks = [
+        ModelCheckpoint("files/model_new.h5"),
+        EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=False)
+    ]
+    train_steps = len(train_X_path) // batch
+    valid_steps = len(val_X_path) // batch
+    if len(train_X_path) % batch != 0:
+        train_steps += 1
+    if len(val_X_path) % batch != 0:
+        valid_steps += 1
 
-cnn_model.fit(
-    train_ds,
-    validation_data=valid_ds,
-    epochs=epochs,
-    steps_per_epoch=train_steps,
-    validation_steps=valid_steps,
-    callbacks=callbacks,
-    shuffle=False
-)
+    cnn_model.fit(
+        train_ds,
+        validation_data=valid_ds,
+        epochs=epochs,
+        steps_per_epoch=train_steps,
+        validation_steps=valid_steps,
+        callbacks=callbacks,
+        shuffle=False
+    )
